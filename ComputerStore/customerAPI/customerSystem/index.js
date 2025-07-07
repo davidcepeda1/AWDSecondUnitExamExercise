d80db7fc633434da1295d642af20b5cd05f74548
Cepeda Salguero David Gustavo <150314536+davidcepeda1@users.noreply.github.com>
2025-07-07 14:53:57 -0500

    som

--- ComputerStore/customerAPI/customerSystem/index.js
+++ ComputerStore/customerAPI/customerSystem/index.js
@@ -1,17 +1,21 @@
-port = 3003
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect(`mongodb+srv://dcepeda:dcepeda@cluster0.fvgdrtu.mongodb.net/ComputerStore?retryWrites=true&w=majority`
-,  {useNewUrlParser: true});
+
+// Use environment variables for port and Mongo URI
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 3003;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
-db.on("Error", (error) => crossOriginIsolated.error(error));
-db.once("Open", ()=> console.log("System connected to MongoDB Database"));
+db.on("Error", (error) => console.error(error));
+db.once("Open", () => console.log("System connected to MongoDB Database"));
 
 app.use(express.json());
 const customerRouter = require("./routes/mouseRoutes");
 
 app.use("/computerstore", customerRouter);
-app.listen(port, () => console.log("My computers store server is running on port -->" + port));
-
+app.listen(port, () => console.log(`My computers store server is running on port --> ${port}`));
